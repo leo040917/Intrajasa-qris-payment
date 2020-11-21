4de9ac4296872cf3beba2727f40c4ffea675372d
leo040917 <72655780+leo040917@users.noreply.github.com>
2020-11-21 11:33:06 +0700

    create project qris

---
+++ src/main/java/com/qris/payment/IntrajasaQrisPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.qris.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntrajasaQrisPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntrajasaQrisPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qris/payment/IntrajasaQrisPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qris.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntrajasaQrisPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
